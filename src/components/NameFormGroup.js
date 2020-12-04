3d81edcf435eca73b13df389937a524c6808e5a6
quobod <quobod@gmail.com>
2020-12-04 17:24:09 -0500

    Working out code for NameFromGroup component

--- src/components/NameFormGroup.js
+++ src/components/NameFormGroup.js
@@ -6,7 +6,13 @@ const NameFormGroup = ({ id, firstName, lastName, modifyProperty }) => {
   const [lname, setLname] = useState('');
   const [bu_fname, setBuFname] = useState('');
   const [bu_lname, setBuLname] = useState('');
-  const [changeOccured, setChangeOccured] = useState(false);
+  const [fnameChangeOccured, setFnameChangeOccured] = useState(false);
+  const [lnameChangeOccured, setLnameChangeOccured] = useState(false);
+  const [fnameChangesSaved, setFnameChangesSaved] = useState(false);
+  const [lnameChangesSaved, setLnameChangesSaved] = useState(false);
+  const [fnameChangesApplied, setFnameChangesApplied] = useState(false);
+  const [lnameChangesApplied, setLnameChangesApplied] = useState(false);
+  const [changes, setChanges] = useState(null);
 
   useEffect(() => {
     setFname(firstName);
@@ -15,17 +21,24 @@ const NameFormGroup = ({ id, firstName, lastName, modifyProperty }) => {
     setBuLname(lastName);
   }, [firstName, lastName]);
 
+  const resetFname = () => setFname(bu_fname);
+
+  const resetLname = () => setLname(bu_lname);
+
   const resetName = () => {
-    setFname(bu_fname);
-    setLname(bu_lname);
-    setChangeOccured(false);
+    resetFname();
+    resetLname();
+    setFnameChangeOccured(false);
+    setLnameChangeOccured(false);
+    setChangesSaved(false);
+    setChangesApplied(false);
   };
 
-  const onFnameChangeHandler = e => {
+  const onFnameChangeHandler = (e) => {
     setFname(e.target.value.trim());
   };
 
-  const onLnameChangeHandler = e => {
+  const onLnameChangeHandler = (e) => {
     setLname(e.target.value.trim());
   };
 
@@ -40,59 +53,79 @@ const NameFormGroup = ({ id, firstName, lastName, modifyProperty }) => {
       <Row>
         <Col xs={12} md={6}>
           <Form.Control
-            className="my-2 mx-auto"
+            className='my-2 mx-auto'
             style={{ background: 'transparent', color: '#fff' }}
-            size="lg"
-            as="input"
-            type="text"
+            size='lg'
+            as='input'
+            type='text'
             value={fname}
             onChange={onFnameChangeHandler}
             onKeyUp={onFnameKeyupHandler}
-            // updateContact
           />
+          {fnameChangeOccured ? 
+            <Row>
+              {!fnameChangesSaved?<Col className='my-3' xs={12} md={6}>
+                <span
+                  onClick={() => console.log(`${id} modified name`)}
+                  className='btn btn-outline-primary d-inline-block border border-primary rounded font-weight-bold'
+                >
+                  <i className='fas fa-pencil-alt fw'></i> Save
+                </span>
+              </Col>:null}
+              <Col className='my-3' xs={12} md={6}>
+                <span
+                  onClick={resetFname}
+                  className={
+                    changeOccured
+                      ? 'btn btn-outline-success d-inline-block border border-success rounded font-weight-bold'
+                      : 'btn btn-outline-light d-inline-block border border-light rounded font-weight-normal'
+                  }
+                >
+                  <i className='fas fa-stop fw'></i> Cancel
+                </span>
+              </Col>
+            </Row>
+           : null}
         </Col>
         <Col xs={12} md={6}>
           <Form.Control
-            className="my-2 mx-auto"
+            className='my-2 mx-auto'
             style={{ background: 'transparent', color: '#fff' }}
-            size="lg"
-            as="input"
-            type="text"
+            size='lg'
+            as='input'
+            type='text'
             // updateContact
             value={lname}
             onChange={onLnameChangeHandler}
             onKeyUp={onLnameKeyupHandler}
           />
+          {lnameChangeOccured ? (
+            <Row>
+             {!} <Col className='my-3' xs={12} md={6}>
+                <span
+                  onClick={() => console.log(`${id} modified name`)}
+                  className='btn btn-outline-primary d-inline-block border border-primary rounded font-weight-bold'
+                >
+                  <i className='fas fa-pencil-alt fw'></i> Save
+                </span>
+              </Col>
+              <Col className='my-3' xs={12} md={6}>
+                <span
+                  onClick={resetName}
+                  className={
+                    changeOccured
+                      ? 'btn btn-outline-success d-inline-block border border-success rounded font-weight-bold'
+                      : 'btn btn-outline-light d-inline-block border border-light rounded font-weight-normal'
+                  }
+                >
+                  <i className='fas fa-stop fw'></i> Cancel
+                </span>
+              </Col>
+            </Row>
+          ) : null}
         </Col>
       </Row>
-      <Row>
-        <Col className="my-3" xs={12} md={6}>
-          <span
-            /* onClick={modifyProperty({
-              id: id,
-              type: 'name',
-              fname: fname,
-              lname: lname,
-            })} */
-            onClick={() => console.log(`${id} modified name`)}
-            className="btn btn-outline-primary d-inline-block border border-primary rounded font-weight-bold"
-          >
-            <i className="fas fa-pencil-alt fw"></i> Save
-          </span>
-        </Col>
-        <Col className="my-3" xs={12} md={6}>
-          <span
-            onClick={resetName}
-            className={
-              changeOccured
-                ? 'btn btn-outline-success d-inline-block border border-success rounded font-weight-bold'
-                : 'btn btn-outline-light d-inline-block border border-light rounded font-weight-normal'
-            }
-          >
-            <i className="fas fa-stop fw"></i> Cancel
-          </span>
-        </Col>
-      </Row>
+      <Row></Row>
     </>
   );
 };
