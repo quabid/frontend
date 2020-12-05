5b4f5b94756f013094ca5167a4b8ddcf057ff0d3
quabid <quabidlord@gmail.com>
2020-12-05 10:38:38 -0500

    Fixed update code

--- src/components/ContactsAccordion.js
+++ src/components/ContactsAccordion.js
@@ -16,79 +16,149 @@ import AddressFormGroup from '../components/AddressFormGroup';
 const ContactsAccordion = ({ contacts }) => {
   const [contactsToUpdate, setContactsToUpdate] = useState([]);
 
-  const updateEntity = (obj) => {
-    switch (obj.action) {
-      case 'update':
-        if (contactsToUpdate.find((x) => x.id === obj.id)) {
-          const contact = contactsToUpdate.find((x) => x.id === obj.id);
+  const updateEntity = obj => {
+    if (obj.id) {
+      const contact = contactsToUpdate.find(x => x.id === obj.id);
+      // If contact is in the list
+      if (contact) {
+        switch (obj.property) {
+          case 'address':
+            contact.addresses.push({
+              category: obj.category || null,
+              address: {
+                street: obj.street || null,
+                city: obj.city || null,
+                zipcode: obj.zipcode || null,
+              },
+            });
+            break;
 
-          switch (obj.property) {
-            case 'address':
-              console.log(`${obj.action} ${obj.property}`);
-              break;
+          case 'email':
+            console.log(`${obj.action} ${obj.property}`);
+            contact.emails.push({
+              category: obj.category || null,
+              email: obj.email || null,
+            });
+            break;
 
-            case 'email':
-              console.log(`${obj.action} ${obj.property}`);
-              break;
+          case 'phone':
+            contact.phones.push({
+              category: obj.category || null,
+              phone: obj.phone || null,
+            });
+            break;
 
-            case 'phone':
-              console.log(`${obj.action} ${obj.property}`);
-              break;
+          case 'name':
+            switch (obj.which) {
+              case 'fname':
+                contact.name.fname = obj.fname;
+                break;
 
-            default:
-              console.log(`Nothing to update`);
-              break;
-          }
-        } else {
-          switch (obj.property) {
-            case 'address':
-              console.log(`${obj.action} ${obj.property}`);
-              break;
+              case 'lname':
+                contact.name.lname = obj.lname;
+                break;
 
-            case 'email':
-              console.log(`${obj.action} ${obj.property}`);
-              break;
+              default:
+                contact.name.fname = null;
+                contact.name.lname = null;
+                break;
+            }
+            break;
 
-            case 'phone':
-              console.log(`${obj.action} ${obj.property}`);
-              break;
-
-            default:
-              console.log(`Nothing to update`);
-              break;
-          }
+          default:
+            console.log(`Nothing to update`);
+            break;
         }
-        break;
+      } else {
+        // If contact is not in the list add it by ID
+        console.log(`${obj.action} ${obj.property}`);
 
-      case 'delete':
-        console.log(`Deleting ${JSON.stringify(obj)}`);
-        break;
+        const contact = {
+          id: obj.id,
+          action: obj.action,
+          emails: [],
+          phones: [],
+          addresses: [],
+          name: { fname: '', lname: '' },
+        };
+        contactsToUpdate.push(contact);
+        switch (obj.property) {
+          case 'address':
+            contact.addresses.push({
+              category: obj.category || null,
+              address: {
+                street: obj.street || null,
+                city: obj.city || null,
+                zipcode: obj.zipcode || null,
+              },
+            });
+            break;
 
-      default:
-        return;
+          case 'email':
+            console.log(`${obj.action} ${obj.property}`);
+            contact.emails.push({
+              category: obj.category || null,
+              email: obj.email || null,
+            });
+            break;
+
+          case 'phone':
+            contact.phones.push({
+              category: obj.category || null,
+              phone: obj.phone || null,
+            });
+            break;
+
+          case 'name':
+            switch (obj.which) {
+              case 'fname':
+                contact.name.fname = obj.fname;
+                break;
+
+              case 'lname':
+                contact.name.lname = obj.lname;
+                break;
+
+              default:
+                contact.name.fname = null;
+                contact.name.lname = null;
+                break;
+            }
+            break;
+
+          default:
+            console.log(`Nothing to update`);
+            break;
+        }
+      }
+    } else {
+      console.log(`\n\n\t\t\tID not provided\n\n`);
+      return;
     }
+    console.log(`\n\n\t\t\tContacts To Be Updated`);
+    console.log(contactsToUpdate);
   };
 
-  const accordion = contacts.map((contact) => {
+  const accordion = contacts.map(contact => {
     return (
       <Card
         key={contact._id}
-        className='text-white my-1'
+        className="text-white my-1"
         style={{ background: 'transparent', fontSize: '1.9rem' }}
       >
         <Accordion.Toggle as={Card.Header} eventKey={contact._id}>
-          <span className='font-weight-bolder text-white'>
+          <span className="font-weight-bolder text-white">
             {contact.fname.substring(0, 1).toUpperCase()}
             {contact.fname.substring(1)}
           </span>
         </Accordion.Toggle>
         <Accordion.Collapse eventKey={contact._id}>
           <Card.Body
-            className='text-white border border-white rounded'
+            className="text-white border border-white rounded"
             style={{ background: 'transparent', fontSize: '1.9rem' }}
           >
             <Jumbotron
-              className='mx-0 my-0 px-1 py-1 text-center text-white'
+              className="mx-0 my-0 px-1 py-1 text-center text-white"
               style={{ background: 'transparent', fontSize: '1rem' }}
             >
               <Row>
@@ -96,12 +166,12 @@ const ContactsAccordion = ({ contacts }) => {
                   {contact.image ? (
                     <CardImg alt={contact.fname} src={contact.image} />
                   ) : (
-                    <i className='fas fa-user fa-8x fw'></i>
+                    <i className="fas fa-user fa-8x fw"></i>
                   )}
                 </Col>
 
                 <Col xs={12} style={{ fontSize: '1.2rem' }}>
-                  <p className='mx-0 my-1'>
+                  <p className="mx-0 my-1">
                     {contact.fname.substring(0, 1).toUpperCase()}
                     {contact.fname.substring(1)}{' '}
                     {contact.lname.substring(0, 1).toUpperCase()}
@@ -111,9 +181,9 @@ const ContactsAccordion = ({ contacts }) => {
 
                 <Col xs={12} style={{ fontSize: '1.2rem' }}>
                   <Form>
-                    <Row className='border border-secondary rounded my-2'>
-                      <Col className='p-3'>
-                        <h2 className='h5 text-left font-weight-bolder'>
+                    <Row className="border border-secondary rounded my-2">
+                      <Col className="p-3">
+                        <h2 className="h5 text-left font-weight-bolder">
                           Name
                         </h2>
                         <NameFormGroup
@@ -124,13 +194,13 @@ const ContactsAccordion = ({ contacts }) => {
                         />
                       </Col>
                     </Row>
-                    <Row className='border border-secondary rounded my-2'>
-                      <Col className='p-3'>
-                        <h2 className='h5 text-left font-weight-bolder'>
+                    <Row className="border border-secondary rounded my-2">
+                      <Col className="p-3">
+                        <h2 className="h5 text-left font-weight-bolder">
                           Phones
                         </h2>
-                        <h2 className='h5 text-right d-inline-block add add-phone'>
-                          <i className='fas fa-plus fw'></i>
+                        <h2 className="h5 text-right d-inline-block add add-phone">
+                          <i className="fas fa-plus fw"></i>
                         </h2>
                         {contact.phones.map((phone, index) => (
                           <PhoneFormGroup
@@ -144,13 +214,13 @@ const ContactsAccordion = ({ contacts }) => {
                         ))}
                       </Col>
                     </Row>
-                    <Row className='border border-secondary rounded my-2'>
-                      <Col className='p-3'>
-                        <h2 className='h5 text-left font-weight-bolder'>
+                    <Row className="border border-secondary rounded my-2">
+                      <Col className="p-3">
+                        <h2 className="h5 text-left font-weight-bolder">
                           Emails
                         </h2>
-                        <h2 className='h5 text-right d-inline-block add add-email'>
-                          <i className='fas fa-plus fw'></i>
+                        <h2 className="h5 text-right d-inline-block add add-email">
+                          <i className="fas fa-plus fw"></i>
                         </h2>
                         {contact.emails.map((email, index) => (
                           <EmailFormGroup
@@ -164,13 +234,13 @@ const ContactsAccordion = ({ contacts }) => {
                         ))}
                       </Col>
                     </Row>
-                    <Row className='border border-secondary rounded my-2'>
-                      <Col className='p-3'>
-                        <h2 className='h5 text-left font-weight-bolder'>
+                    <Row className="border border-secondary rounded my-2">
+                      <Col className="p-3">
+                        <h2 className="h5 text-left font-weight-bolder">
                           Addresses
                         </h2>
-                        <h2 className='h5 text-right d-inline-block add add-address'>
-                          <i className='fas fa-plus fw'></i>
+                        <h2 className="h5 text-right d-inline-block add add-address">
+                          <i className="fas fa-plus fw"></i>
                         </h2>
                         {contact.addresses.map((address, index) => (
                           <AddressFormGroup
--- src/components/NameFormGroup.js
+++ src/components/NameFormGroup.js
@@ -21,9 +21,19 @@ const NameFormGroup = ({ id, firstName, lastName, modifyProperty }) => {
     setBuLname(lastName);
   }, [firstName, lastName]);
 
-  const resetFname = () => setFname(bu_fname);
+  const resetFname = () => {
+    setFname(bu_fname);
+    setFnameChangeOccured(false);
+    setFnameChangesApplied(false);
+    setFnameChangesSaved(false);
+  };
 
-  const resetLname = () => setLname(bu_lname);
+  const resetLname = () => {
+    setLname(bu_lname);
+    setLnameChangeOccured(false);
+    setLnameChangesApplied(false);
+    setLnameChangesSaved(false);
+  };
 
   const resetName = () => {
     resetFname();
@@ -36,124 +46,165 @@ const NameFormGroup = ({ id, firstName, lastName, modifyProperty }) => {
     setLnameChangesApplied(false);
   };
 
-  const onFnameChangeHandler = (e) => {
+  const onFnameChangeHandler = e => {
     setFname(e.target.value.trim());
   };
 
   const onFnameKeyupHandler = () =>
     setFnameChangeOccured(fname.trim() !== bu_fname.trim() ? true : false);
 
-  const onLnameChangeHandler = (e) => {
+  const saveFnameProperty = () => {
+    if (changes) {
+      changes.fanme = fname;
+    } else {
+      setChanges({
+        id: id,
+        action: 'update',
+        property: 'name',
+        which: 'fname',
+        fname: fname,
+      });
+    }
+    setFnameChangesSaved(true);
+  };
+
+  const onLnameChangeHandler = e => {
     setLname(e.target.value.trim());
   };
 
   const onLnameKeyupHandler = () =>
     setLnameChangeOccured(lname.trim() !== bu_lname.trim() ? true : false);
 
-  const applyChanges = () => {
-    console.log(`Changes applied`);
+  const saveLnameProperty = () => {
+    if (changes) {
+      changes.lanme = lname;
+    } else {
+      setChanges({
+        id: id,
+        action: 'update',
+        property: 'name',
+        which: 'lname',
+        lname: lname,
+      });
+    }
+    setLnameChangesSaved(true);
+  };
+
+  const applyFnameChanges = () => {
+    resetFname();
+  };
+
+  const applyLnameChanges = () => {
+    resetLname();
   };
 
   return (
-    <>
-      <Row>
-        <Col xs={12} md={6}>
-          <Form.Control
-            className='my-2 mx-auto'
-            style={{ background: 'transparent', color: '#fff' }}
-            size='lg'
-            as='input'
-            type='text'
-            value={fname}
-            onChange={onFnameChangeHandler}
-            onKeyUp={onFnameKeyupHandler}
-          />
-          {fnameChangeOccured ? (
-            <Row>
-              {!fnameChangesSaved ? (
-                <Col className='my-3' xs={12} md={6}>
-                  <span
-                    onClick={() => console.log(`${id} modified name`)}
-                    className='btn btn-outline-primary d-inline-block border border-primary rounded font-weight-bold'
-                  >
-                    <i className='fas fa-pencil-alt fw'></i> Save
-                  </span>
-                </Col>
-              ) : null}
-
-              {!fnameChangesApplied ? (
-                <Col className='my-3' xs={12} md={3}>
-                  <span
-                    onClick={() => {
-                      applyChanges();
-                      changes &&
-                        changes.action === 'update' &&
-                        modifyProperty(changes);
-                    }}
-                    className='btn btn-outline-success d-inline-block border border-success rounded font-weight-bold'
-                  >
-                    <i className='fas fa-go fw'></i> Apply
-                  </span>
-                </Col>
-              ) : null}
-
-              <Col className='my-3' xs={12} md={6}>
+    <Row>
+      <Col xs={12} md={6}>
+        <Form.Control
+          className="my-2 mx-auto"
+          style={{ background: 'transparent', color: '#fff' }}
+          size="lg"
+          as="input"
+          type="text"
+          value={fname}
+          onChange={onFnameChangeHandler}
+          onKeyUp={onFnameKeyupHandler}
+        />
+        {fnameChangeOccured ? (
+          <>
+            {!fnameChangesSaved ? (
+              <Col className="my-3" xs={12} md={6}>
                 <span
-                  onClick={resetFname}
-                  className={
-                    fnameChangeOccured
-                      ? 'btn btn-outline-success d-inline-block border border-success rounded font-weight-bold'
-                      : 'btn btn-outline-light d-inline-block border border-light rounded font-weight-normal'
-                  }
+                  onClick={saveFnameProperty}
+                  className="btn btn-outline-primary d-inline-block border border-primary rounded font-weight-bold"
                 >
-                  <i className='fas fa-stop fw'></i> Cancel
+                  <i className="fas fa-pencil-alt fw"></i> Save
                 </span>
               </Col>
-            </Row>
-          ) : null}
-        </Col>
-        <Col xs={12} md={6}>
-          <Form.Control
-            className='my-2 mx-auto'
-            style={{ background: 'transparent', color: '#fff' }}
-            size='lg'
-            as='input'
-            type='text'
-            // updateContact
-            value={lname}
-            onChange={onLnameChangeHandler}
-            onKeyUp={onLnameKeyupHandler}
-          />
-          {lnameChangeOccured ? (
-            <Row>
-              {!lnameChangesSaved ? (
-                <Col className='my-3' xs={12} md={6}>
-                  <span
-                    onClick={() => console.log(`${id} modified name`)}
-                    className='btn btn-outline-primary d-inline-block border border-primary rounded font-weight-bold'
-                  >
-                    <i className='fas fa-pencil-alt fw'></i> Save
-                  </span>
-                </Col>
-              ) : null}
-              <Col className='my-3' xs={12} md={6}>
+            ) : null}
+
+            {fnameChangesSaved ? (
+              <Col className="my-3" xs={12} md={3}>
                 <span
-                  onClick={resetLname}
-                  className={
-                    lnameChangeOccured
-                      ? 'btn btn-outline-success d-inline-block border border-success rounded font-weight-bold'
-                      : 'btn btn-outline-light d-inline-block border border-light rounded font-weight-normal'
-                  }
+                  onClick={() => {
+                    applyFnameChanges();
+                    changes &&
+                      changes.action === 'update' &&
+                      modifyProperty(changes);
+                  }}
+                  className="btn btn-outline-success d-inline-block border border-success rounded font-weight-bold"
                 >
-                  <i className='fas fa-stop fw'></i> Cancel
+                  <i className="fas fa-go fw"></i> Apply
                 </span>
               </Col>
-            </Row>
-          ) : null}
-        </Col>
-      </Row>
-      <Row></Row>
-    </>
+            ) : null}
+
+            <Col className="my-3" xs={12} md={6}>
+              <span
+                onClick={resetFname}
+                className="btn btn-outline-success d-inline-block border border-success rounded font-weight-bold"
+              >
+                <i className="fas fa-stop fw"></i> Cancel
+              </span>
+            </Col>
+          </>
+        ) : null}
+      </Col>
+
+      <Col xs={12} md={6}>
+        <Form.Control
+          className="my-2 mx-auto"
+          style={{ background: 'transparent', color: '#fff' }}
+          size="lg"
+          as="input"
+          type="text"
+          // updateContact
+          value={lname}
+          onChange={onLnameChangeHandler}
+          onKeyUp={onLnameKeyupHandler}
+        />
+        {lnameChangeOccured ? (
+          <>
+            {!lnameChangesSaved ? (
+              <Col className="my-3" xs={12} md={6}>
+                <span
+                  onClick={saveLnameProperty}
+                  className="btn btn-outline-primary d-inline-block border border-primary rounded font-weight-bold"
+                >
+                  <i className="fas fa-pencil-alt fw"></i> Save
+                </span>
+              </Col>
+            ) : null}
+
+            {lnameChangesSaved ? (
+              <Col className="my-3" xs={12} md={3}>
+                <span
+                  onClick={() => {
+                    applyLnameChanges();
+                    changes &&
+                      changes.action === 'update' &&
+                      modifyProperty(changes);
+                  }}
+                  className="btn btn-outline-success d-inline-block border border-success rounded font-weight-bold"
+                >
+                  <i className="fas fa-go fw"></i> Apply
+                </span>
+              </Col>
+            ) : null}
+
+            <Col className="my-3" xs={12} md={6}>
+              <span
+                onClick={resetLname}
+                className="btn btn-outline-success d-inline-block border border-success rounded font-weight-bold"
+              >
+                <i className="fas fa-stop fw"></i> Cancel
+              </span>
+            </Col>
+          </>
+        ) : null}
+      </Col>
+    </Row>
   );
 };
 
